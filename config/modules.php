e38d28f20a3f55541a0b360eb256fcf77e4a69e4
Dmitry Khlystov <dkhlystov@gmail.com>
2016-07-18 00:01:47 +1000

    SEO module added

--- config/modules.php
+++ config/modules.php
@@ -4,5 +4,6 @@ return [
 	'page' => 'page\backend\Module',
 	'menu' => 'menu\backend\Module',
 	'gallery' => 'gallery\backend\Module',
+	'seo' => 'seo\backend\Module',
 	'user' => 'user\backend\Module',
 ];
