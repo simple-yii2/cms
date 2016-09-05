2b9f654f69d66951045fd819f3322c76c3264708
Dmitry Khlystov <dkhlystov@gmail.com>
2016-09-05 23:34:37 +1000

    Slider added to config

--- config/modules.php
+++ config/modules.php
@@ -4,6 +4,7 @@ return [
 	'page' => 'page\backend\Module',
 	'menu' => 'menu\backend\Module',
 	'gallery' => 'gallery\backend\Module',
+	'slider' => 'slider\backend\Module',
 	'seo' => 'seo\backend\Module',
 	'user' => 'user\backend\Module',
 ];
