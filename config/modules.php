7d343e883cd37072dee96ad3c0f49716c0c8984f
Dmitry Khlystov <dkhlystov@gmail.com>
2016-12-21 16:49:25 +1000

    block module namespace changed

--- config/modules.php
+++ config/modules.php
@@ -3,11 +3,11 @@
 return [
 	'menu' => 'cms\menu\backend\Module',
 	'page' => 'page\backend\Module',
-	'blocks' => 'simple\blocks\backend\Module',
+	'block' => 'cms\block\backend\Module',
 	'gallery' => 'gallery\backend\Module',
 	'slider' => 'slider\backend\Module',
 	'contacts' => 'simple\contacts\backend\Module',
 	'seo' => 'seo\backend\Module',
-	
+
 	'user' => 'cms\user\backend\Module',
 ];
