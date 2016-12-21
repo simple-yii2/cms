2335d37577c41b133d17033a7db237268600f71e
Dmitry Khlystov <dkhlystov@gmail.com>
2016-12-21 15:27:42 +1000

    menu namespace changed

--- config/modules.php
+++ config/modules.php
@@ -1,12 +1,13 @@
 <?php
 
 return [
+	'menu' => 'cms\menu\backend\Module',
 	'page' => 'page\backend\Module',
-	'menu' => 'menu\backend\Module',
 	'blocks' => 'simple\blocks\backend\Module',
 	'gallery' => 'gallery\backend\Module',
 	'slider' => 'slider\backend\Module',
 	'contacts' => 'simple\contacts\backend\Module',
 	'seo' => 'seo\backend\Module',
+	
 	'user' => 'cms\user\backend\Module',
 ];
