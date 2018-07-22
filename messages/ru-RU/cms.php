ec84482795b469f8c67fdbe36c3480493bf638a2
Dmitry Khlystov <dkhlystov@gmail.com>
2018-07-22 17:44:54 +1000

    messages file update

--- messages/ru-RU/cms.php
+++ messages/ru-RU/cms.php
@@ -5,4 +5,14 @@ return [
 
 	'The above error occurred while the Web server was processing your request.' => 'Произошла ошибка во время обработки сервером вашего запроса.',
 	'Please contact us if you think this is a server error. Thank you.' => 'Пожалуйста, свяжитесь с нами, если вы считаете, что это ошибка сервера. Спасибо.',
+
+
+
+    'Create' => 'Добавить',
+    'Cancel' => 'Отмена',
+    'Save' => 'Сохранить',
+
+    'Changes saved successfully.' => 'Изменения успешно сохранены.',
+    'Item not found.' => 'Объект не найден.',
+    'Item deleted successfully.' => 'Объект успешно удален.',
 ];
